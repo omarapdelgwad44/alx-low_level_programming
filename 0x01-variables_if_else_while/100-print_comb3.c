c9b44a04df6fa9d9023b78f2990c5ee5280ff713
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2023-03-20 01:40:54 +0200

    'suiiii'

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,28 +6,24 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i < 100; i++)
-{
-for (j = i + 1; j < 100; j++)
-{
-bool lucy=true,x=i;
-if (i != j && i / 10 < j / 10 && !(i == 0 && j == 1))
-{
-natsu:
-putchar(x / 10 + '0');
-putchar(x % 10 + '0');
-if(lucy)
-{
-putchar(',');
-putchar(' ');
-x=j;
-lucy = false;
-goto natsu;
-}
-}
-}
-}
-putchar('\n');
-return (0);
+    int i, j;
+
+    for (i = 0; i < 9; i++)
+    {
+        for (j = i + 1; j < 10; j++)
+        {
+            putchar((i % 10) + '0');
+            putchar((j % 10) + '0');
+
+            if (i == 8 && j == 9)
+                continue;
+
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
 }
