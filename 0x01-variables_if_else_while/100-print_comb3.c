824b4ee8f2eecf46529a69580f838e82df48f02b
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2023-03-20 01:15:42 +0200

    'suiiii'

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ for (i = 0; i < 100; i++)
 {
 for (j = i + 1; j < 100; j++)
 {
-if (i != j)
+if (i != j && i / 10 < j / 10 && !(i == 0 && j == 1))
 {
-if (i != 0 || j != 1)
-{
-putchar(',',' ');
-}
-putchar((i / 10 + '0'),(i % 10 + '0'));
-putchar((i / 10 + '0'),(i % 10 + '0'));
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(',');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
 }
 }
 }
