1a7079f2575837d110e95a44d8d4516b5e5d70bc
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2023-03-20 01:37:10 +0200

    'suiiii'

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,20 @@ for (i = 0; i < 100; i++)
 {
 for (j = i + 1; j < 100; j++)
 {
+bool lucy=true,x=i;
 if (i != j && i / 10 < j / 10 && !(i == 0 && j == 1))
 {
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
+natsu:
+putchar(x / 10 + '0');
+putchar(x % 10 + '0');
+if(lucy)
+{
 putchar(',');
 putchar(' ');
+x=j;
+lucy = false;
+goto natsu;
+}
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
 }
