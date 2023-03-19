d3b9d678e5d5033dc166e59af9bbc7da630a9948
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2023-03-20 00:08:23 +0200

    'suiiii'

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers of base 10 starting from 0
+ * Return: 0 if program executes properly, else a non-zero number
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+}
+putchar('\n');
+return (0);
+}
