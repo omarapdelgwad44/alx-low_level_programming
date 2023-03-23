b0121b0921cf525d9a6ffb65e0a071212257b218
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2023-03-23 06:48:41 -0700

    suiiiiiii

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+    return (c >= 'A' && c <= 'Z');
+}
