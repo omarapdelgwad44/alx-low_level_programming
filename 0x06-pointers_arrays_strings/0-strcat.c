b57ecb50771c4ab7dae4ecbf99f5c9a7898cacd5
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2023-03-29 15:20:41 -0700

    suiii

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,23 @@
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * strcat - Concatenates the string pointed to by @src, including the x                                                     
- *          null byte, to the end of the string pointed to by @dest.                                                                 
- * @dest: A pointer to the string to be concatenated upon.                                                                           
- * @src: The source string to be appended to @dest.                                                                                  
- *                                                                                                                                   
- * Return: A pointer to the destination string @dest.                                                                                
- */                                                                                                                                  
-char *_strcat(char *dest, const char *src)                                                                                            
-{                                                                                                                                    
-        int index = 0, dest_len = 0;                                                                                                 
-                                                                                                                                     
-        while (dest[index++])                                                                                                        
-                dest_len++;                                                                                                          
-                                                                                                                                     
-        for (index = 0; src[index]; index++)                                                                                         
-                dest[dest_len++] = src[index];                                                                                       
-                                                                                                                                     
-        return (dest);                                                                                                               
-}} 
+julien@ubuntu:~/c/curriculum_by_julien/holbertonschool-low_level_programming/0x06$ cat 0-main.c
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
